29b40c1931dec2c020bd42a0803a3be83d89474e
Jeet Patel <jp2398d@gmail.com>
2022-05-19 14:16:19 +0530

    adding default

---
+++ solidityDefaults.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+solidity_auto() {
+
+    # initialize project
+    sudo npm init -y
+
+    #  install dependencies
+    sudo npm install dotenv 
+    sudo npm install hardhat --save-dev 
+    sudo npx hardhat 
+    sudo npm install --save-dev @nomiclabs/hardhat-ethers ethers @nomiclabs/hardhat-waffle ethereum-waffle chai
+
+    # adding content for plugins
+    sed -i '1i require("dotenv").config();' hardhat.config.js
+    sed -i '1i require("@nomiclabs/hardhat-waffle");' hardhat.config.js
+    
+    #  create folders and create scripts
+    sudo mkdir contracts/ test/
+    sudo mkdir -p scripts/deploy.js
+    sudo touch .env
+
+    #  create a .gitignore and add files
+    sudo echo ".gitignore" >> .gitignore
+    sudo echo ".env" >> .gitignore
+    sudo echo "node_modules" >> .gitignore
+}
+solidity_auto
